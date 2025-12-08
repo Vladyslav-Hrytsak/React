25521a4da10a5c189858ba09310d1b66788ba087
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2025-12-08 18:40:36 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import MyComponent from "./MyComponents/MyComponent.tsx";
+
+function App() {
+
+  return (
+    <>
+        <MyComponent text="Hello World!" />
+        <MyComponent text="Hello Vlad!" />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/MyComponents/MyComponent.tsx
@@ -0,0 +1,11 @@
+import type {FC} from "react";
+import styles from "./MyComponent.module.css";
+
+type MyComponentPropType = { text: string }
+
+const  MyComponent:FC<MyComponentPropType> = ({text} )=> {
+    return <div className={ styles.target }>{text}</div>
+}
+
+
+export default MyComponent;
