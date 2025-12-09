8546c0c6d4f74d777bad45f4368592be447eb65b
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2025-12-09 15:15:35 +0200

    homework-1/task2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
+import CoursesComponent from "./components/courses-component/CoursesComponent.tsx";
 
 function App() {
 
   return (
-    <>
-
-
-    </>
+    <CoursesComponent/>
   )
 }
 
 export default App
+
---
+++ src/models/CourseModel.ts
@@ -0,0 +1,1 @@
+export type CourseModel =  {title: string, monthDuration: number}
