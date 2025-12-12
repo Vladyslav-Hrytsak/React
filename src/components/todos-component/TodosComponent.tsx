431ce404b019469d48068e7095cf68865d749062
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2025-12-12 17:39:17 +0200

    hw task-1

---
+++ src/components/todos-component/TodosComponent.tsx
@@ -0,0 +1,17 @@
+import {useEffect} from "react";
+
+const TodosComponent = () => {
+    useEffect(() => {
+        
+    }, []);
+
+    return (
+        <div>
+            {
+
+            }
+        </div>
+    );
+};
+
+export default TodosComponent;
