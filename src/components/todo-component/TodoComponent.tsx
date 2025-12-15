598669562a3324f81ac873bf7eeb53a6faf634af
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2025-12-15 14:51:39 +0200

    hw-2 task-3 dummyjson fix key

--- src/components/todo-component/TodoComponent.tsx
+++ src/components/todo-component/TodoComponent.tsx
@@ -7,7 +7,7 @@ interface TodoComponentProps {
 
 const TodoComponent:FC<TodoComponentProps> = ({item:{id,userId,completed,todo}}: TodoComponentProps) => {
     return (
-        <div key={id} className='border-2 m-4 p-4 text-center bg-blue-100 rounded-lg '>
+        <div className='border-2 m-4 p-4 text-center bg-blue-100 rounded-lg '>
             <h2 className='text-2xl'> {todo}{id}</h2>
             <p>{userId}</p>
             <p className='text-4xl'>{completed.toString()}</p>
