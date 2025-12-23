e199329549bbf15d9daf00021034002f6dc19aed
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2025-12-23 16:54:28 +0200

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
-import PostsComponent from "./components/posts-component/PostsComponent.tsx";
 
 function App() {
 
   return (
     <>
-        <PostsComponent/>
     </>
   )
 }
